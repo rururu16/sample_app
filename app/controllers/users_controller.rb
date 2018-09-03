bdc46f598ad72eb18e83784f9019b2ab7f6ccc71
EC2 Default User <ynk618172@gmail.com>
2018-09-03 12:40:10 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,12 @@ class UsersController < ApplicationController
   def create
     @user =User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "ようこそ！"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "メールを確認してください"
+      redirect_to root_url
+      # log_in @user
+      # flash[:success] = "ようこそ！"
+      # redirect_to @user
     else
       render 'new'
     end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  # メールプレビュー用設定（11.2.2）
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'rails-tutorial-mhartl.c9users.io'  
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+
 end
