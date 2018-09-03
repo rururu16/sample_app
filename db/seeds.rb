f9e23825c729ae2ab1796c62652537dc87294919
EC2 Default User <ynk618172@gmail.com>
2018-09-03 15:28:00 +0900

    誤字

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-               activates: true,
+               activated: true,
                activated_at: Time.zone.now
                )
 end
