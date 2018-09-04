3bd97dcb95a5413e8bdf826bd02baeac6fc89657
EC2 Default User <ynk618172@gmail.com>
2018-09-04 16:39:49 +0900

    13.3.2~

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,19 +58,12 @@ class UsersController < ApplicationController
                                     :password, :password_confirmation)
     end
     
-    def logged_in_user
-      unless logged_in?
-      store_location
-        flash[:danger] = "ログインしてください"
-        redirect_to login_url
-      end
-    end
-    
+    # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-    
+    # 管理者かどうか確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
