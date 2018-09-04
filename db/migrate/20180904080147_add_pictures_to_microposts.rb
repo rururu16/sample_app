b04af137cef1f2a05a03ed52101fe88eca86b1b3
EC2 Default User <ynk618172@gmail.com>
2018-09-04 17:20:26 +0900

    13.4.2~

--- db/migrate/20180904080147_add_pictures_to_microposts.rb
+++ db/migrate/20180904080147_add_pictures_to_microposts.rb
@@ -1,4 +1,4 @@
-class AddPictureToMicroposts < ActiveRecord::Migration[5.1]
+class AddPicturesToMicroposts < ActiveRecord::Migration[5.1]
   def change
     add_column :microposts, :picture, :string
   end
